80dd3f9a23d3c383622b839bd84223ad3b278e01
Alan Ho <aho0593@gmail.com>
2023-04-03 22:05:20 +0000

    dom-querying - Alan Ho

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -2,11 +2,16 @@ const $heading = document.querySelector('h1');
 console.log('$heading', $heading);
 console.dir($heading);
 
-console.log('$explanation', document.querySelector('#explanation'));
-console.dir(document.querySelector('#explanation'));
+const $explanation = document.querySelector('#explanation');
+console.log('$explanation', $explanation);
+console.dir($explanation);
 
-console.log('$hint', document.querySelector('.hint'));
-console.dir(document.querySelector('.hint'));
+const $hint = document.querySelector('.hint');
+console.log('$hint', $hint);
+console.dir($hint);
 
-console.log('$paragraph', document.querySelectorAll('p'));
-console.log('$example-link', document.querySelectorAll('.example-link'));
+const $paragraph = document.querySelectorAll('p');
+console.log('$paragraph', $paragraph);
+
+const $exampleLink = document.querySelectorAll('.example-link');
+console.log('$example-link', $exampleLink);
