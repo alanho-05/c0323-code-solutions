09dce85785d5189f569bdeda1a42d783b71f51f6
Alan Ho <aho0593@gmail.com>
2023-04-05 18:09:53 +0000

    javascript-forms revised - Alan Ho

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -3,9 +3,9 @@ const input = {};
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
-  input['name:'] = $form.elements.name.value;
-  input['email:'] = $form.elements.email.value;
-  input['message:'] = $form.elements.message.value;
+  input.name = $form.elements.name.value;
+  input.email = $form.elements.email.value;
+  input.message = $form.elements.message.value;
   console.log('messageData:', input);
-  this.reset();
+  $form.reset();
 });
