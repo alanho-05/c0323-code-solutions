191296bee1782a145cb1efc79c6e4e294b56c91c
Alan Ho <aho0593@gmail.com>
2023-04-07 21:53:42 +0000

    javascript-local-storage - Alan Ho

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,18 @@
+/* exported todos */
+
+let todos = [];
+const previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+const $window = document.defaultView;
+console.log($window);
+
+$window.addEventListener('beforeunload', callback);
+
+function callback(event) {
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
