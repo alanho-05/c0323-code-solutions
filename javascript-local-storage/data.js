34df28a8fc5005ab7a33cc5a17644ccb1fedadd7
Alan Ho <aho0593@gmail.com>
2023-04-07 22:00:55 +0000

    javascript-local-storage revised - Alan Ho

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -7,9 +7,7 @@ if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
 }
 
-const $window = document.defaultView;
-
-$window.addEventListener('beforeunload', callback);
+window.addEventListener('beforeunload', callback);
 
 function callback(event) {
   const todosJSON = JSON.stringify(todos);
