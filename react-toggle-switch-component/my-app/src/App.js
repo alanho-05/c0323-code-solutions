c527c274d2f1468519e4c626a6eabe25347b5a74
Alan Ho <aho0593@gmail.com>
2023-05-22 18:14:54 +0000

    work in progress

---
+++ react-toggle-switch-component/my-app/src/App.js
@@ -0,0 +1,14 @@
+import ToggleSwitch from './ToggleSwitch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ToggleSwitch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-toggle-switch-component/my-app/src/ToggleSwitch.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function ToggleSwitch() {
+  return (
+    <>
+      <label>
+        <input type="checkbox" />
+      </label>
+    </>
+  );
+}
