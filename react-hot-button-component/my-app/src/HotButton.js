1000077a21672a5eb16887fd8b2b328f1621a380
Alan Ho <aho0593@gmail.com>
2023-05-22 17:50:30 +0000

    react-hot-button-component - Alan Ho

--- react-hot-button-component/my-app/src/HotButton.js
+++ react-hot-button-component/my-app/src/HotButton.js
@@ -1,12 +1,33 @@
 import './HotButton.css';
 import { useState } from 'react';
 
-export default function HotButton({ color }) {
+export default function HotButton({ text }) {
   const [index, setIndex] = useState(0);
 
   function handleClick() {
     setIndex(index + 1);
   }
 
-  return <button></button>;
+  let btnColor;
+
+  if (index < 6) {
+    btnColor = 'pur';
+  } else if (index < 9) {
+    btnColor = 'lightPur';
+  } else if (index < 12) {
+    btnColor = 'red';
+  } else if (index < 15) {
+    btnColor = 'orange';
+  } else if (index < 18) {
+    btnColor = 'yellow';
+  } else btnColor = 'white';
+
+  return (
+    <>
+      <button onClick={handleClick} className={btnColor}>
+        {text}
+      </button>
+      <span>{index} Clicks</span>
+    </>
+  );
 }
