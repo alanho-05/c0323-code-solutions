87183a0aca2f42e4624cc328ba5b09d131180c45
Alan Ho <aho0593@gmail.com>
2023-05-23 18:08:12 +0000

    react-validated-input-component - Alan Ho

---
+++ react-validated-input-component/my-app/src/ValidatedInput.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export default function ValidatedInput() {
+  const [password, setPassword] = useState('');
+
+  let validPassLength = password.length >= 8;
+  let validPass = validPassLength ? 'is-valid' : 'is-invalid';
+  let errText =
+    password.length === 0
+      ? 'A password is required.'
+      : 'Your password is too short.';
+
+  return (
+    <form>
+      <div className="form-group position-relative">
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          className={`form-control ${validPass}`}
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div
+          style={{ backgroundColor: 'white', color: 'red' }}
+          className="invalid-tooltip">
+          {errText}
+        </div>
+      </div>
+    </form>
+  );
+}
