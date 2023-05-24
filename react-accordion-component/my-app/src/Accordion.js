0782f32ac9f61a76e2508363c28e0af66121f73f
Alan Ho <aho0593@gmail.com>
2023-05-24 19:26:05 +0000

    react-accordion-component revised

--- react-accordion-component/my-app/src/Accordion.js
+++ react-accordion-component/my-app/src/Accordion.js
@@ -25,23 +25,24 @@ export default function Accordion({ topic }) {
 
 /**
  * A component that creates the textbox for languages and their description
- * @param prop1 {text}: a string value for each topic
- * @param prop2 {description}: a string value for each topic's description
- * @param prop3 {active}: a Boolean value to signal if topic's description should be showing
- * @param prop4 {onShow}: an event handler function to signal to parent component it has been clicked on
+ * @param {string} text: a string value for each topic
+ * @param {string} description: a string value for each topic's description
+ * @param {Boolean} active: a Boolean value to signal if topic's description should be showing
+ * @param {function} onshow: an event handler function to signal to parent component it has been clicked on
  * @returns the created text elements
  */
 
 function Subject({ text, description, active, onShow }) {
-  const show = active ? '' : 'hidden';
   return (
     <>
       <div className="textbox m-auto graybg" onClick={onShow}>
         <h2>{text}</h2>
       </div>
-      <div className={`textbox m-auto ${show}`}>
-        <p>{description}</p>
-      </div>
+      {active && (
+        <div className="textbox m-auto">
+          <p>{description}</p>
+        </div>
+      )}
     </>
   );
 }
