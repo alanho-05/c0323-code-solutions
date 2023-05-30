e1dd8b8332cd8d745dc1a5ca6cf234551cfa4c88
Alan Ho <aho0593@gmail.com>
2023-05-30 21:56:33 +0000

    fetch revised

--- fetch/main.js
+++ fetch/main.js
@@ -13,8 +13,8 @@ async function fetchPokemon() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
     if (!response.ok) throw new Error(`{fetch Error ${response.status}`);
-    const Array = await response.json();
-    console.log(Array);
+    const array = await response.json();
+    console.log(array);
   } catch (err) {
     console.log('fetch failed!', err);
   }
