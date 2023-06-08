39fc6dccb69187f81a8ffc54758bee6eb36e3a4a
Alan Ho <aho0593@gmail.com>
2023-06-08 17:36:43 +0000

    First challenge completed

--- data-structures-queues/take-2nd.js
+++ data-structures-queues/take-2nd.js
@@ -1,1 +1,10 @@
-export default function take2nd(queue) {}
+export default function take2nd(queue) {
+  if (queue.peek() === undefined) {
+    return;
+  }
+  for (let i = 0; i < 1; i++) {
+    const storedQueue = queue.dequeue();
+    queue.enqueue(storedQueue);
+  }
+  return queue.dequeue();
+}
